8f4af6cafba5ad340d819b56b49aaeda9bc2cfca
chalypeng <chalypeng@gmail.com>
2017-10-15 16:47:35 +0800

    before launch mode

--- ActivityTest/app/src/main/java/xyz/toor/chalypeng/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/xyz/toor/chalypeng/activitytest/FirstActivity.java
@@ -1,8 +1,10 @@
 package xyz.toor.chalypeng.activitytest;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,10 +12,12 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
+    private static final String TAG = "FirstActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, this.toString());
         setContentView(R.layout.first_layout);
 
         Button button1 = (Button) findViewById(R.id.button_1);
@@ -23,27 +27,55 @@ public class FirstActivity extends AppCompatActivity {
 //                Toast.makeText(FirstActivity.this,"You clicked Button 1!",Toast.LENGTH_SHORT)
 //                        .show();
 //                finish();
-                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+//                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+//                Intent intent = new Intent("xyz.toor.chalypeng.activitytest.ACTION_START");
+//                intent.addCategory("xyz.toor.chalypeng.activitytest.MY_CATEGORY");
+                // 调用浏览器
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("http://www.baidu.com"));
+                // 调用电话拨打电话
+//                Intent intent = new Intent(Intent.ACTION_DIAL);
+//                intent.setData(Uri.parse("tel:10086"));
+//                String data = "Hello SecondActivity";
+//                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+//                intent.putExtra("extra_data",data);
+
+//                startActivity(intent);
+//                startActivityForResult(intent,1);
+                // 启动模式，启动自己
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
                 startActivity(intent);
             }
-
         });
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d(TAG, returnedData);
+                }
+                break;
+            default:
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main,menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.add_item:
-                Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.remove_item:
-                Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
                 break;
             default:
         }
--- ActivityTest/app/src/main/java/xyz/toor/chalypeng/activitytest/SecondActivity.java
+++ ActivityTest/app/src/main/java/xyz/toor/chalypeng/activitytest/SecondActivity.java
@@ -1,13 +1,37 @@
 package xyz.toor.chalypeng.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+    private static final String TAG = "SecondActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, this.toString());
         setContentView(R.layout.second_layout);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return","Hello FirstActivity");
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data_return","Hello FirstActivity");
+        setResult(RESULT_OK,intent);
+        finish();
     }
 }
